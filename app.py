cdbc61a35b479192e86ac8799c552250741f62ef
unknown <mini@marvin.skynet.com>
2021-12-11 23:14:28 -0500

    Formatting

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-import os, requests, json, pytz
-from flask import Flask, abort, render_template
+import json
+import os
+import pytz
+import requests
 from datetime import datetime
+from flask import Flask, abort, render_template
+from typing import Any
 
 base_url = "https://www.espn.com"
 
@@ -11,31 +15,37 @@ leagues = {
 	"nhl": "nhl",
 	"ncaam": "mens-college-basketball",
 	"ncaaf": "college-football",
-};
+}
 
 app = Flask(__name__)
+
 
 @app.route(os.environ['WEB_ROOT'])
 def index():
 	try:
 		return render_template('index.html', WEB_ROOT=os.environ['WEB_ROOT'])
-	except:
+	except Exception as e:
+		logger.error('Error on line {} {} {}'.format(sys.exc_info()[-1].tb_lineno, type(e).__name__, e))
 		abort(404)
-		
+
+
 @app.route(os.environ['WEB_ROOT'] + '<league>/', defaults={'game_date': None})
 @app.route(os.environ['WEB_ROOT'] + '<league>/<game_date>/')
 def scoreboard(league, game_date):
 	try:
-		if game_date is None: game_date = datetime.today().replace(tzinfo=pytz.UTC).astimezone(pytz.timezone(os.environ['TIME_ZONE'])).strftime("%Y%m%d")
+		if game_date is None:
+			game_date = datetime.today().replace(tzinfo=pytz.UTC).astimezone(
+				pytz.timezone(os.environ['TIME_ZONE'])).strftime("%Y%m%d")
 		datetime.strptime(game_date, '%Y%m%d')
 	except ValueError:
 		return "Invalid Date Format. Should be YYYYMMDD"
-		
+
 	try:
 		espn_league = leagues[league]
-	except:
+	except Exception as e:
+		logger.error('Error on line {} {} {}'.format(sys.exc_info()[-1].tb_lineno, type(e).__name__, e))
 		return "Invalid League. Please choose MLB, NBA, NFL, NHL, NCAAM or NCAAF"
-		
+
 	if league == 'nfl':
 		week_num = datetime.strptime(game_date, '%Y%m%d').isocalendar()[1] - datetime.strptime(os.environ['NFL_WEEK_1_START'], '%Y%m%d').isocalendar()[1] + 1
 		if datetime.strptime(os.environ['NFL_WEEK_1_START'], '%Y%m%d') > datetime.strptime(game_date, '%Y%m%d'):
@@ -46,36 +56,45 @@ def scoreboard(league, game_date):
 		url = base_url + "/" + espn_league + "/scoreboard/_/year/2020/seasontype/" + season + "/week/" + str(week_num)
 	else:
 		url = base_url + "/" + espn_league + "/scoreboard/_/date/" + game_date
-	
+
 	data = requests.get(url)
 	if league == 'nhl':
-		json_data = cleanNHL(data)
+		json_data = clean_nhl(data)
 	else:
-		json_data = cleanDefault(data)
+		json_data = clean_default(data)
 
 	return json.dumps(json_data)
-	
-def cleanDefault(data):
-	start = data.text.find("<script>window.espn.scoreboardData")+38
+
+
+def clean_default(data):
+	start = data.text.find("<script>window.espn.scoreboardData") + 38
 	end = data.text.find("</script>", start - 1)
-	data = data.text[start:end].replace(";","").replace("window.espn.scoreboardData","").replace("if(!window.espn_ui.device.isMobile){window.espn.loadType = \"ready\"};","").split("window.espn.scoreboardSettings")[0].strip()
+	data = data.text[start:end].replace(";", "").replace("window.espn.scoreboardData", "").replace(
+		"if(!window.espn_ui.device.isMobile){window.espn.loadType = \"ready\"};", "").split(
+		"window.espn.scoreboardSettings")[0].strip()
 	try:
 		json_data = json.loads(data)['events']
+		game: Any
 		for game in json_data:
 			game['teams'] = {}
-			for x in range(0,2):
-				game['teams'][game['competitions'][0]['competitors'][x]['homeAway']] = game['competitions'][0]['competitors'][x]['team']
-				game['teams'][game['competitions'][0]['competitors'][x]['homeAway']]['score'] = game['competitions'][0]['competitors'][x]['score']
+			for x in range(0, 2):
+				game['teams'][game['competitions'][0]['competitors'][x]['homeAway']] = \
+					game['competitions'][0]['competitors'][x]['team']
+				game['teams'][game['competitions'][0]['competitors'][x]['homeAway']]['score'] = \
+					game['competitions'][0]['competitors'][x]['score']
 				if 'records' in game['competitions'][0]['competitors'][x]:
 					for teams in game['competitions'][0]['competitors'][x]['records']:
-						if teams['type'] == 'total': game['teams'][game['competitions'][0]['competitors'][x]['homeAway']]['standing'] = game['competitions'][0]['competitors'][x]['records'][0]['summary']
-			
+						if teams['type'] == 'total':
+							game['teams'][game['competitions'][0]['competitors'][x]['homeAway']]['standing'] = \
+								game['competitions'][0]['competitors'][x]['records'][0]['summary']
+
 			game['status']['state'] = game['status']['type']['state']
 			game['status']['detail'] = game['status']['type']['detail']
 			game['status']['shortDetail'] = game['status']['type']['shortDetail']
 			game['status']['completed'] = game['status']['type']['completed']
-			
-			if len(game['competitions'][0]['broadcasts']) > 0: game['broadcast'] = game['competitions'][0]['broadcasts'][0]
+
+			if len(game['competitions'][0]['broadcasts']) > 0:
+				game['broadcast'] = game['competitions'][0]['broadcasts'][0]
 			del game['season']
 			del game['uid']
 			del game['id']
@@ -83,65 +102,89 @@ def cleanDefault(data):
 			del game['competitions']
 			del game['status']['type']
 			del game['status']['displayClock']
-			
-			for x in ['home','away']:
+
+			for x in ['home', 'away']:
 				del game['teams'][x]['links']
 				del game['teams'][x]['id']
 				del game['teams'][x]['uid']
 				del game['teams'][x]['isActive']
-				if 'name' in game['teams'][x]: del game['teams'][x]['name']
-	except:
+				if 'name' in game['teams'][x]:
+					del game['teams'][x]['name']
+	except Exception as e:
+		logger.error('Error on line {} {} {}'.format(sys.exc_info()[-1].tb_lineno, type(e).__name__, e))
 		json_data = {}
 
 	return json_data
-	
-def cleanNHL(data):
-	start = data.text.find(" <script type='text/javascript' >window['__espnfitt__']")+56
+
+
+def clean_nhl(data):
+	start = data.text.find(" <script type='text/javascript' >window['__espnfitt__']") + 56
 	end = data.text.find("</script>", start - 1)
-	data = data.text[start:end].replace(";","").strip()
+	data = data.text[start:end].replace(";", "").strip()
 	try:
 		json_data = json.loads(data)['page']['content']['scoreboard']['events'][0]
 		for game in json_data:
 			game['teams'] = {}
-			for x in range(0,2):
+			for x in range(0, 2):
 				try:
 					game['teams']['home' if game['competitors'][x]['isHome'] else 'away'] = game['competitors'][x]
 					if 'records' in game['competitors'][x]:
 						for teams in game['competitors'][x]['records']:
-							if teams['type'] == 'total': game['teams']['home' if game['competitors'][x]['isHome'] else 'away']['standing'] = game['competitors'][x]['records'][0]['summary']
-				except:
-					pass
-			
+							if teams['type'] == 'total':
+								game['teams']['home' if game['competitors'][x]['isHome'] else 'away']['standing'] = \
+									game['competitors'][x]['records'][0]['summary']
+				except Exception as e:
+					logger.error('Error on line {} {} {}'.format(sys.exc_info()[-1].tb_lineno, type(e).__name__, e))
+
 			game["name"] = game['teams']['away']['displayName'] + " at " + game['teams']['home']['displayName']
 			game["shortName"] = game['teams']['away']['abbrev'] + " @ " + game['teams']['home']['abbrev']
-			
-			if len(game['broadcasts']) > 0: game['broadcast'] = game['broadcasts'][0]
+
+			if len(game['broadcasts']) > 0:
+				game['broadcast'] = game['broadcasts'][0]
 			del game['id']
 			del game['competitors']
-			if 'tbd' in game: del game['tbd']
-			if 'link' in game: del game['link']
-			if 'note' in game: del game['note']
-			if 'completed' in game: del game['completed']
-			if 'tickets' in game: del game['tickets']
-			if 'links' in game: del game['links']
-			if 'hideScoreDate' in game: del game['hideScoreDate']
-			if 'teamInfo' in game: del game['teamInfo']
-			if 'allStar' in game: del game['allStar']
-			if 'odds' in game: del game['odds']
-			if 'highlights' in game: del game['highlights']
-			if 'day' in game: del game['day']
-			if 'month' in game: del game['month']
-			if 'watchListen' in game: del game['watchListen']
-			if 'leaders' in game: del game['leaders']
-			if 'gameTimeFormat' in game: del game['gameTimeFormat']
-			if 'performerTitle' in game: del game['performerTitle']
-			if 'time' in game: del game['time']
-			
+			if 'tbd' in game:
+				del game['tbd']
+			if 'link' in game:
+				del game['link']
+			if 'note' in game:
+				del game['note']
+			if 'completed' in game:
+				del game['completed']
+			if 'tickets' in game:
+				del game['tickets']
+			if 'links' in game:
+				del game['links']
+			if 'hideScoreDate' in game:
+				del game['hideScoreDate']
+			if 'teamInfo' in game:
+				del game['teamInfo']
+			if 'allStar' in game:
+				del game['allStar']
+			if 'odds' in game:
+				del game['odds']
+			if 'highlights' in game:
+				del game['highlights']
+			if 'day' in game:
+				del game['day']
+			if 'month' in game:
+				del game['month']
+			if 'watchListen' in game:
+				del game['watchListen']
+			if 'leaders' in game:
+				del game['leaders']
+			if 'gameTimeFormat' in game:
+				del game['gameTimeFormat']
+			if 'performerTitle' in game:
+				del game['performerTitle']
+			if 'time' in game:
+				del game['time']
+
 			del game['status']['id']
 			del game['status']['description']
 			game['status']['shortDetail'] = game['status']['detail']
 
-			for x in ['home','away']:
+			for x in ['home', 'away']:
 				del game['teams'][x]['recordSummary']
 				del game['teams'][x]['standingSummary']
 				del game['teams'][x]['isHome']
@@ -149,15 +192,16 @@ def cleanNHL(data):
 				del game['teams'][x]['records']
 				del game['teams'][x]['id']
 				del game['teams'][x]['uid']
-			
+
 				game['teams'][x]['abbreviation'] = game['teams'][x]['abbrev']
 				game['teams'][x]['alternateColor'] = game['teams'][x]['altColor']
 				game['teams'][x]['color'] = game['teams'][x]['teamColor']
-				
+
 				del game['teams'][x]['abbrev']
 				del game['teams'][x]['altColor']
 				del game['teams'][x]['teamColor']
-	except:
+	except Exception as e:
+		logger.error('Error on line {} {} {}'.format(sys.exc_info()[-1].tb_lineno, type(e).__name__, e))
 		json_data = {}
 
 	return json_data
